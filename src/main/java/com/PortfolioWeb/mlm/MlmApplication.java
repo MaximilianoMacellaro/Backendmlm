dd975f4f3ae5c65351e800a389dca6726eee4d10
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-06-28 19:57:45 -0300

    Dependecias back

--- src/main/java/com/PortfolioWeb/mlm/MlmApplication.java
+++ src/main/java/com/PortfolioWeb/mlm/MlmApplication.java
@@ -15,11 +15,11 @@ public class MlmApplication {
 		SpringApplication.run(MlmApplication.class, args);
 	}
 
-        @Bean
+@Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://frontend-2715c.web.app"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "ACA PONES TU URL DE FIREBASE"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
