24f71b0f04bfcaf00f644a9fffe14bc8a9a8af62
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-06-28 20:46:13 -0300

    Corss

--- src/main/java/com/PortfolioWeb/mlm/MlmApplication.java
+++ src/main/java/com/PortfolioWeb/mlm/MlmApplication.java
@@ -19,7 +19,7 @@ public class MlmApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "ACA PONES TU URL DE FIREBASE"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://frontend-2715c.web.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
