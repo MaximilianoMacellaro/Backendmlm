d389ed322a7367107d5b1c07a9997941c008389f
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-06-29 14:44:06 -0300

    Arreglo persona service

--- src/main/java/com/PortfolioWeb/mlm/Controler/PersonaController.java
+++ src/main/java/com/PortfolioWeb/mlm/Controler/PersonaController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = {"https://frontend-2715c.web.app"})
+@RequestMapping("/personas")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     @GetMapping("/personas/traer")
