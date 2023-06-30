39ab7e39b43543720baf714361735f4a35e8cbd6
MaximilianoMacellaro <leonelmacellaro@gmail.com>
2023-06-30 13:03:17 -0300

    Persona controller

--- src/main/java/com/PortfolioWeb/mlm/Service/ImpPersonaService.java
+++ src/main/java/com/PortfolioWeb/mlm/Service/ImpPersonaService.java
@@ -1,41 +1,42 @@
 package com.PortfolioWeb.mlm.Service;
 
 import com.PortfolioWeb.mlm.Entity.Persona;
-import com.PortfolioWeb.mlm.Interface.IPersonaService;
 import com.PortfolioWeb.mlm.Repository.IPersonaRepository;
 import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service 
-public class ImpPersonaService implements IPersonaService{
+@Transactional
+public class ImpPersonaService{
     @Autowired IPersonaRepository ipersonaRepository;
-
-    @Override
-    public List<Persona> getPersona() {
-        List<Persona> persona = ipersonaRepository.findAll();
-        return persona;
-    }
-
-    @Override
-    public void savePersona(Persona persona) {
-        ipersonaRepository.save(persona);
-    }
-
-    @Override
-    public void deletePersona(Long id) {
-        ipersonaRepository.deleteById(id);
-    }
-
-    @Override
-    public Persona findPersona(Long id) {
-        Persona persona = ipersonaRepository.findById(id).orElse(null);
-        return persona;
-    }
-
-    @Override
-    public void save(Persona persona) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+ public List<Persona> list(){
+         return ipersonaRepository.findAll();
+     }
+     
+     public Optional<Persona> getOne(int id){
+         return ipersonaRepository.findById(id);
+     }
+     
+     public Optional<Persona> getByNombre(String nombre){
+         return ipersonaRepository.findByNombre(nombre);
+     }
+     
+     public void save(Persona persona){
+         ipersonaRepository.save(persona);
+     }
+     
+     public void delete(int id){
+         ipersonaRepository.deleteById(id);
+     }
+     
+     public boolean existsById(int id){
+         return ipersonaRepository.existsById(id);
+     }
+     
+     public boolean existsByNombre(String nombre){
+         return ipersonaRepository.existsByNombre(nombre);
+     }
 }
